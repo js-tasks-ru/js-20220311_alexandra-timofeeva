db7c625e2a875719fd0c9548a56effec8a868bb9
atimofeeva <welcomeToApt1>
2022-03-20 18:51:00 +0100

    Remove unused code

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -8,7 +8,6 @@ export const pick = (obj, ...fields) => {
   const result = {};
   Object.entries(obj).forEach(([key, value]) => {
     if (fields.includes(key)) {
-      // result [key] = value;
       result[key] = key;
     }
   });
