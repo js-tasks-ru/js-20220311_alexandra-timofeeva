47a37ee69fdeb06e5f0a30e3af79f6d7d0af1edb
atimofeeva <welcomeToApt1>
2022-03-25 16:52:30 +0100

    Create function for returning array of uniq values

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,20 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
+import index from "../../01-intro/1-sum";
 
+export function uniq(arr) {
+  return [...new Set(arr)];
 }
+
+// Another solution:
+// export function uniq(arr) {
+//   const result = [];
+//   arr.forEach(item => {
+//     if (result.indexOf(item) < 0)
+//     {result.push(item);}
+//   });
+//   return result;
+// }
+
+
