93c6b0162d3ffd10862104076f31725d9ce914ef
atimofeeva <welcomeToApt1>
2022-03-27 20:09:50 +0200

    Create function for removing consecutive identical symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,25 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) { return '';}
+  if (size === undefined) { return string;}
+  if (string === '') { return string;}
 
+  let previousChar = '';
+  let count = 0;
+  let newString = '';
+
+  for (const char of string) {
+    if (char === previousChar) {
+      count++;
+    }
+    else {
+      count = 0;
+    }
+    if (count < size) {
+      newString += char;
+    }
+    previousChar = char;
+  }
+  return newString;
 }
