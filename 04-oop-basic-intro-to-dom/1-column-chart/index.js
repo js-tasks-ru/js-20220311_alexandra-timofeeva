f818fa3848d50a80bb85975a292c27db0f2a1d07
atimofeeva <welcomeToApt1>
2022-03-30 15:33:32 +0200

    Create column-chart

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,83 @@
 export default class ColumnChart {
+  subElements = {};
+  chartHeight = 50;
 
+  data = [];
+  label = '';
+  link = '';
+  value = 0;
+
+  constructor({ data = [], label = 0, link = '', value = 0, formatHeading = data => data } = {}) {
+    this.data = data;
+    this.label = label;
+    this.link = link;
+    this.value = formatHeading(value);
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+      <div class="column-chart column-chart_loading" style=${this.chartHeight}>
+        <div class="column-chart__title">
+          Total ${this.label}
+          ${this.link ? `<a href=${this.link} class="column-chart__link">View all</a>` : ''}
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">${this.value}</div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.getColumnBody()}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+    if (this.data.length) {
+      this.element.classList.remove('column-chart_loading');
+    }
+    this.subElements = this.getSubElements();
+  }
+
+  update() {}
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
+  }
+
+  getColumnBody() {
+    const maxValue = Math.max(...this.data);
+    const scale = this.chartHeight / maxValue;
+
+    return this.data
+      .map(item => {
+        const percent = ((item / maxValue) * 100).toFixed(0);
+
+        return `<div style="--value: ${Math.floor(
+          item * scale
+        )}" data-tooltip="${percent}%"></div>`;
+      })
+      .join("");
+  }
+
+  getSubElements() {
+    const result = {};
+    const elements = this.element.querySelectorAll('[data-element]');
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+    return result;
+  }
 }
