e51d82596791579534bb31ed10f407112cc88d1f
atimofeeva <welcomeToApt1>
2022-04-05 21:58:28 +0200

    Add basic structure

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,30 @@
 export default class NotificationMessage {
+  element;
+
+  constructor() {
+    this.render();
+  }
+
+  getTemplate() {
+    return ``;
+  }
+
+  render() {
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
+  }
+
+
+
 
 }
